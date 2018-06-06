fcefa610613eafd63e39148f19af3ff1792988e7
Ivan Sued <thefenry@gmail.com>
2018-06-06 16:25:41 -0700

    adding basic skeleton to filemanager

---
+++ src/app/file-manager/file-manager.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FileManagerComponent } from './components/file-manager/file-manager.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [FileManagerComponent]
+})
+export class FileManagerModule { }
